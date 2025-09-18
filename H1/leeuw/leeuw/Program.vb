377598084ecaaf5b6b49543c52f00b8e0f783e55
Joris  Custers | SFE Personeel <joris.custers@sfevergem.be>
2025-09-18 12:18:37 +0200

    adding leeuw project for first checkin

---
+++ H1/leeuw/leeuw/Program.vb
@@ -0,0 +1,17 @@
+Imports System
+
+
+
+
+Module Program
+
+    Class Leeuw
+        Public intSnelheid As Integer
+        Public intLeeftijd As Integer
+    End Class
+
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+
+    End Sub
+End Module
